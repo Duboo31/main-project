616e31931d9509fadba05ec5de8c0e7e2db7d68a
Duboo31 <duboo31@gmail.com>
2022-09-28 19:23:06 +0900

    config 폴더 생성

--- server/index.js
+++ server/index.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://hyun0310woo:hyun1857@cluster0.9domrkr.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! 노드몬 성공");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입에 필요한 정보를 클라에서 가져오면
+  // 그것들을 DB에 넣어준다
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
